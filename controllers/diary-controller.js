3495bf21b40a155ba5ddc834060068c0e8a0288d
OlgaGresko <122181692+OlgaGresko@users.noreply.github.com>
2023-11-15 01:54:52 +0200

    fixed adding empty array

--- controllers/diary-controller.js
+++ controllers/diary-controller.js
@@ -21,44 +21,42 @@ const addDiary = async (req, res) => {
       const foundExercise = await Exercise.findById(exercise);
 
       const newExercise = {
-        exercise, 
-        time, 
+        exercise,
+        time,
         calories,
         bodyPart: foundExercise.bodyPart,
-        equipment:foundExercise.equipment,
+        equipment: foundExercise.equipment,
         name: foundExercise.name,
         target: foundExercise.target,
-      }
-   
+      };
+
       updatedDoneExercises.push(newExercise);
       burnedCalories += calories;
-    
-  }
-  update.$addToSet = { doneExercises: { $each: updatedDoneExercises } };
-}
-
-if (consumedProducts && consumedProducts.length > 0) {
-
-  const updatedConsumedProducts = [];
-  for (const productObj of consumedProducts) {
-    const { product, amount, calories } = productObj;
-    const foundProduct = await Product.findById(product);
-    const newProduct = {
-      product, 
-      amount, 
-      calories,
-      title: foundProduct.title,
-      category: foundProduct.category,
-      groupBloodNotAllowed: foundProduct.groupBloodNotAllowed[blood],
     }
- 
-    updatedConsumedProducts.push(newProduct);
-    consumedCalories += calories;
-  
-}
-update.$addToSet = { consumedProducts: { $each: updatedConsumedProducts } };
-}
- update.$inc = { burnedCalories, consumedCalories };
+    update.$addToSet = { doneExercises: { $each: updatedDoneExercises } };
+  }
+
+  if (consumedProducts && consumedProducts.length > 0) {
+    const updatedConsumedProducts = [];
+    for (const productObj of consumedProducts) {
+      const { product, amount, calories } = productObj;
+      const foundProduct = await Product.findById(product);
+      const newProduct = {
+        product,
+        amount,
+        calories,
+        title: foundProduct.title,
+        category: foundProduct.category,
+        groupBloodNotAllowed: foundProduct.groupBloodNotAllowed[blood],
+      };
+
+      updatedConsumedProducts.push(newProduct);
+      consumedCalories += calories;
+    }
+    update.$addToSet = { consumedProducts: { $each: updatedConsumedProducts } };
+  }
+
+  update.$inc = { burnedCalories, consumedCalories };
 
   const options = { new: true, upsert: true };
   const result = await Diary.findOneAndUpdate(conditions, update, options);
