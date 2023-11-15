985d8a8d79e70de7ccc5ceaa5a5ba90e4e7acc0f
Artem <117488314+ArtemBortyaniy@users.noreply.github.com>
2023-11-15 15:26:11 +0200

    add socket

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
 import mongoose from "mongoose";
 import app from "./app.js";
+//soket
+import { Server } from "socket.io";
+import { createServer } from "http";
+import User from "./models/User.js";
+import Exercise from "./models/Exercise.js";
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("New frontend connection");
+
+  socket.on("getUsers", async () => {
+    try {
+      const allUsers = await User.find();
+      io.emit("totalUsers", allUsers.length);
+    } catch (error) {
+      console.error("Error querying MongoDB:", error.message);
+    }
+  });
+});
+
+httpServer.listen(5050, () => {
+  console.log("listening on *:5050");
+});
 
 const { DB_URI, PORT = 3000 } = process.env;
 
